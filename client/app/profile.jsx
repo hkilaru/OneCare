cde19f940f320c0caf9a81d449fd04453099e08d
Harish Kilaru <hckilaru@gmail.com>
2016-08-06 16:56:39 -0400

    added bootstrap

--- client/app/profile.jsx
+++ client/app/profile.jsx
@@ -85,7 +85,7 @@ export default class Profile extends React.Component {
       <Navigate />
       <h1> My Profile </h1>
           <div className="allScripts">
-      <button onClick={this.openModal}> Enter New Prescription </button>
+      <button onClick={this.openModal}> Add New Prescription </button>
 
       <Modal
         isOpen={this.state.modalIsOpen}
@@ -95,7 +95,7 @@ export default class Profile extends React.Component {
           <button onClick={this.closeModal}>Exit</button>
       </Modal>
 
-      <h2> Profile {window.localStorage.username} </h2>
+      <h2> Prescriptions </h2>
 
              {
               this.state.scripts.map((script, idx) => {
@@ -104,12 +104,8 @@ export default class Profile extends React.Component {
                   <div className="single-script">
                     <li> <span className="user-script"> Name: </span> {script.name} </li>
                     <li> <span className="user-script"> Dosage: </span> {script.dosage} </li>
-                    <li> <span className="user-script"> Frequency </span> {script.frequency} </li>
-                    <li> <span className="user-script"> Recurring </span> {script.recur} </li>
-                    <li> <span className="user-script"> Refill Date </span> {script.refill} </li>
-                    <li> <span className="user-script"> Refill Reminder </span> {script.refillRemind} </li>
-                    <li> <span className="user-script"> Refill Reminder </span> {script.dailyRemind} </li>
-                    <li> <span className="user-script"> Phone: </span> {script.phone} </li>
+                    <li> <span className="user-script"> Frequency: </span> {script.frequency} </li>
+                    <li> <span className="user-script"> Refill Date: </span> {script.refill} </li>
                   </div>
                  <button onClick={this.deleteReminder.bind(this, idx)} value={idx}>Delete</button>
                  </ul>
